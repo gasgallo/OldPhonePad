a3c7bd0d9aa0041141dbb47fba51406f6115fbf9
Riki <rgall@sertiscorp.com>
2023-05-08 18:21:19 +0700

    docs: add docstrings

--- OldPhonePad/OldPhonePad.cs
+++ OldPhonePad/OldPhonePad.cs
@@ -2,8 +2,14 @@
 
 namespace IronSW;
 
+/// <summary>
+/// Class <c>OldPhonePadTranslator</c> translates key strokes from an old phone keypad into a string.
+/// </summary>
 public class OldPhonePadTranslator
 {
+    /// <summary>
+    /// Dictionary <c>OLDPAD_MAP</c> maps each key stroke with the generated character.
+    /// </summary>
     private static readonly Dictionary<char, string> OLDPAD_MAP = new Dictionary<char, string>() {
         {'1', "&'("},
         {'2', "ABC"},
@@ -17,44 +23,47 @@ public class OldPhonePadTranslator
         {'0', " "}
     };
 
+    /// <summary>
+    /// Method <c>OldPhonePad</c> gets a sequence of symbold from an old phone pad as an input and returns the translated string.
+    /// </summary>
     public static string OldPhonePad(string input)
     {
         StringBuilder output = new StringBuilder();
         char prevChar = ' ';
         int consecutiveCount = 0;
 
+        /// Iterate over input
         foreach (char c in input)
         {
-            if (c == '#')
+            if (c == '#')  /// end of input
             {
                 break;
             }
-            else if (c == '*')
+            else if (c == '*')  /// delete key
             {
-                if (output.Length > 0)
+                if (output.Length > 0)  /// delete makes sense only if there's something to delete
                 {
                     output.Remove(output.Length - 1, 1);
                     prevChar = ' ';
                 }
             }
-            else if (c == ' ')
+            else if (c == ' ')  /// reset previous character after stroke pause
             {
                 prevChar = ' ';
             }
             else
             {
-                if (c == prevChar)
+                if (c == prevChar)  /// the same key has been pressed more than once in a row
                 {
-                    if (consecutiveCount == OLDPAD_MAP[c].Length)
+                    if (consecutiveCount == OLDPAD_MAP[c].Length)  /// reset count based on the number of characters available for each key
                     {
                         consecutiveCount = 0;
                     }
 
-                    output.Remove(output.Length - 1, 1);
-                    output.Append(OLDPAD_MAP[c][consecutiveCount]);
+                    output[output.Length - 1] = OLDPAD_MAP[c][consecutiveCount];
                     consecutiveCount++;
                 }
-                else
+                else   /// new key
                 {
                     output.Append(OLDPAD_MAP[c][0]);
                     prevChar = c;
